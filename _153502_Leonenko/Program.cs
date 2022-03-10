6494a2a4ae34d9f1ae860292488dcc7c55d3d8ee
Aleksei Leonenko <alx.leonenko@gmail.com>
2022-03-10 12:18:30 +0300

    Added the 1st lab

---
+++ _153502_Leonenko/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _153502_Leonenko
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+        {
+			Console.WriteLine("Enter two numbers: ");
+			int a = Convert.ToInt32(Console.ReadLine());
+			int b = Convert.ToInt32(Console.ReadLine());
+			int res = a / b;
+			Console.WriteLine($"Result of division is {res}");
+        }
+	}
+}
